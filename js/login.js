f432d6b1be87c33f4a8328460fd3385dd9c9b7f6
mohammadamzad38 <hossainamzad38@gmail.com>
2024-10-09 18:35:37 +0600

    some

--- js/login.js
+++ js/login.js
@@ -1,9 +1,19 @@
 document.getElementById('log-button')
 .addEventListener('click', function(event){
     event.preventDefault();
-    console.log('loginbutton click  ')
 
     // phone number input
     const phonenumber = document.getElementById('phone-number').value;
-    console.log(phonenumber, 'paisi')
+    // pin number input
+    const pinnumber = document.getElementById('pin-number').value;
+
+    console.log(phonenumber, pinnumber)
+
+    if(phonenumber === '5' && pinnumber === '1234'){
+        console.log('You are loged in')
+        window.location.href= '/home.html';
+    }
+    else{
+        alert('Wrong input')
+    }
 })
