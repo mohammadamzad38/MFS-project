00a34b806226d1292db1df6298d75e0ce853a991
mohammadamzad38 <hossainamzad38@gmail.com>
2024-10-10 16:40:08 +0600

    shesh

--- js/features.js
+++ js/features.js
@@ -3,3 +3,10 @@ document.getElementById('cash-out-option-button').addEventListener('click', func
     document.getElementById('cash-out-form').classList.remove('hidden');
     document.getElementById('add-money-form').classList.add('hidden');
 })
+
+document.getElementById('add-moneyoption-button').addEventListener('click', function(){
+    document.getElementById('add-money-form').classList.remove('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+
+    
+})
