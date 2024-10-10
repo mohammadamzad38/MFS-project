6299c8c2abbbbd42dc3a156e8d10beb118ebead6
mohammadamzad38 <hossainamzad38@gmail.com>
2024-10-10 12:07:59 +0600

    hobe

--- js/home.js
+++ js/home.js
@@ -1,9 +1,25 @@
 // Add money to the account
 
-document.getElementById('bt  n-add-money').addEventListener('click', function(event){
+document.getElementById('btn-add-money').addEventListener('click', function(event){
     event.preventDefault();
-    
+
     const addmoneyinput = document.getElementById('input-add-money').value; 
     const inputpinnumber = document.getElementById('pin-number-input').value;
-    console.log(addmoneyinput, inputpinnumber)
+    
+    // step 3 verify pin
+    if(inputpinnumber === '1234'){
+
+        // 04 --get current balance
+        const balance = document.getElementById('account-balance').innerText;
+
+        // 05 --add money input with balance convert
+        const addmoneynumber = parseFloat(addmoneyinput);
+        const balancenumber = parseFloat(balance);
+        const newbalance = addmoneynumber + balancenumber;
+
+        document.getElementById('account-balance').innerText = newbalance;
+    }
+    else{
+        alert ('please try again')
+    }
 })
