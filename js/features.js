4fcf6c703aa2163ec39fe716866c50bd65eee066
mohammadamzad38 <hossainamzad38@gmail.com>
2024-10-10 16:23:28 +0600

    si update

---
+++ js/features.js
@@ -0,0 +1,5 @@
+document.getElementById('cash-out-option-button').addEventListener('click', function(event){
+
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    document.getElementById('add-money-form').classList.add('hidden');
+})
